a0905eea0d1fd24adaac2b98ebbaabb9b3163cb8
surybang <hos.fabien@outlook.com>
2025-08-28 20:21:29 +0000

    feat(delete): time the execution with decorator

--- data_pipeline/delete.py
+++ data_pipeline/delete.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
+from decorators import timed
 
+
+@timed("Delete file")
 def delete_local_file(path: Path) -> bool:
     """Delete local file. Returns True if deleted, False if missing."""
     try:
